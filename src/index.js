1291221e31d40bbdf9e7738560ffb95f8cc762a6
themtrx <themtrx@mail.bg>
2023-09-21 17:12:30 +0300

    Restart game if bird touches borders.

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ function preload () {
 
 const VELOCITY = 200
 const flapVelocity = 250
+const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2 } 
 
 let bird = null
 
@@ -35,18 +36,21 @@ function create () {
   const backgroung = this.add.image(0, 0, 'sky')
   backgroung.setOrigin(0)
 
-  bird = this.physics.add.sprite(config.width * 0.1 , config.height * 0.5, 'bird').setOrigin(0)
+  bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0)
 
   this.input.keyboard.on('keydown-SPACE', flap);
 }
 
-
 function update (time, delta) {
-  if(bird.y > config.height){
-    console.log('game over height')
-  }else if(bird.y < 0){
-    console.log('game over dropped')
+  if(bird.y > config.height || bird.y < -bird.height){
+    restartPlayerPosition()
   }
+}
+
+function restartPlayerPosition(){
+  bird.x = initialBirdPosition.x
+  bird.y = initialBirdPosition.y
+  bird.body.velocity.y = 0
 }
 
 function flap(){
