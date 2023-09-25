d65a8930c4591fd8a627c7c4f9b5bc046710dd47
themtrx <themtrx@mail.bg>
2023-09-25 11:14:00 +0300

    Pipes recycle.

--- src/index.js
+++ src/index.js
@@ -23,12 +23,15 @@ new Phaser.Game(config);
 const VELOCITY = 200
 const PIPE_TO_RENDER = 4
 
+let bird = null
+let pipes = null
+
 let pipeHorizontalDistance = 0
+const pipeOpeningRange = [150, 250]
+const pipeHorizontalDistanceRange = [500, 550]
 
 const flapVelocity = 250
 const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2 } 
-
-let bird = null
 
 function preload () {
   this.load.image('sky', 'assets/sky.png')
@@ -43,9 +46,11 @@ function create () {
   bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0)
   bird.body.gravity.y = 400
 
+  pipes = this.physics.add.group()
+
   for (let i = 0; i < PIPE_TO_RENDER; i++) {
-    const upperPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 1)
-    const lowerPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0)
+    const upperPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 1)
+    const lowerPipe = pipes.create(0, 0, 'pipe').setOrigin(0)
 
     placePipes(upperPipe, lowerPipe)
   }
@@ -57,6 +62,8 @@ function update (time, delta) {
   if(bird.y > config.height || bird.y < -bird.height){
     restartPlayerPosition()
   }
+
+  recyclePipes()
 }
 
 function restartPlayerPosition(){
@@ -70,14 +77,12 @@ function flap(){
 }
 
 function placePipes(upPipe, lowPipe){
-  pipeHorizontalDistance += 400
+  const rightMostX = getRightMostPipe()
+  const pipeOpening = Phaser.Math.Between(...pipeOpeningRange)
+  const pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeOpening)
+  const pipeHorizontalDistance = Phaser.Math.Between(...pipeHorizontalDistanceRange)
 
-  let pipeOpeningRange = [150, 250]
-  let pipeOpening = Phaser.Math.Between(...pipeOpeningRange)
-
-  let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeOpening)
-
-  upPipe.x = pipeHorizontalDistance
+  upPipe.x = rightMostX + pipeHorizontalDistance
   upPipe.y = pipeVerticalPosition
 
   lowPipe.x = upPipe.x
@@ -85,4 +90,28 @@ function placePipes(upPipe, lowPipe){
 
   upPipe.body.velocity.x = -200
   lowPipe.body.velocity.x = -200
+}
+
+function recyclePipes(){
+  const tempPipes = []
+
+  pipes.getChildren().forEach(pipe => {
+    if(pipe.getBounds().right < 0){
+      tempPipes.push(pipe)
+
+      if(tempPipes.length == 2){
+        placePipes(...tempPipes)
+      }
+    }
+  })
+}
+
+function getRightMostPipe(){
+  let rightMostX = 0
+
+  pipes.getChildren().forEach(pipe => {
+    rightMostX = Math.max(pipe.x, rightMostX)
+  })
+
+  return rightMostX
 }
