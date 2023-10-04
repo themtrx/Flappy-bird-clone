458cccb7d4439868f9c10d98d720c454c130121c
themtrx <themtrx@mail.bg>
2023-10-04 11:29:08 +0300

    Show menu elements.

--- src/scene/MenuScene.js
+++ src/scene/MenuScene.js
@@ -4,10 +4,17 @@ class MenuScene extends BaseScene {
 
     constructor(config) {
         super('MenuScene', config)
+
+        this.menu = [
+            { scene: 'PlayScene', text: 'Play' },
+            { scene: 'ScoreScene', text: 'Score' },
+            { scene: null, text: 'Exit' }
+        ]
     }
 
     create() {
         super.create()
+        this.createMenu(this.menu)
     }
 }
 
